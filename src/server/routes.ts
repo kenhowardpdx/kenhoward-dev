31492610f7df798960b392f46950de75020ac57c
Ken Howard <ken@simplicatedweb.com>
2020-12-08 20:25:58 -0800

    feat: render post page

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -3,6 +3,7 @@ import Hapi from '@hapi/hapi'
 import Server from './server'
 import handleIndex from './handle_index'
 import handlePosts from './handle_posts'
+import handlePost from './handle_post'
 
 const defineRoute = (server: Server, handler: (server: Server, req: Hapi.Request, h: Hapi.ResponseToolkit) => Hapi.ResponseObject | Promise<Hapi.ResponseObject>, method: string, path: string): Hapi.ServerRoute => {
   const serverHandler: Hapi.Lifecycle.Method = (req: Hapi.Request, h: Hapi.ResponseToolkit): Hapi.ResponseObject | Promise<Hapi.ResponseObject> => handler(server, req, h)
@@ -16,7 +17,8 @@ const defineRoute = (server: Server, handler: (server: Server, req: Hapi.Request
 const configureRoutes = (server: Server): Hapi.ServerRoute[] => {
   return [
     defineRoute(server, handleIndex, 'GET', '/'),
-    defineRoute(server, handlePosts, 'GET', '/posts')
+    defineRoute(server, handlePosts, 'GET', '/posts'),
+    defineRoute(server, handlePost, 'GET', '/posts/{p*}')
   ]
 }
 
